ba7b3d032f75db54146d080675c94d625a5a3217
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-04-28 12:12:44 +0300

    Intial commit to github

---
+++ src/main/java/guru/radugacode256/spring5webappv1/Spring5WebAppv1Application.java
@@ -0,0 +1,13 @@
+package guru.radugacode256.spring5webappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebAppv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5WebAppv1Application.class, args);
+    }
+
+}
---
+++ src/test/java/guru/radugacode256/spring5webappv1/Spring5WebAppv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.radugacode256.spring5webappv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebAppv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
